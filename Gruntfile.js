e6fdd28f8c38467c4ede46cf6275692135fcb822
Christian Rudh <christian@primian.se>
2014-01-07 12:23:08 +0100

    Updated indentation.

--- Gruntfile.js
+++ Gruntfile.js
@@ -1,51 +1,51 @@
 module.exports = function(grunt) {
-  var config = {
-    pkg: grunt.file.readJSON("package.json"),
+    var config = {
+        pkg: grunt.file.readJSON("package.json"),
 
-    connect: {
-      server: {
-        options: {
-          port: 8080,
-          base: "./src",
-          keepalive: true
-        }
-      }
-    },
-
-    react: {
-      options: {
-        extension: ".jsx",
-        ignoreMTime: false
-      },
-      files: {
-        expand: true,
-        cwd: "./src/app",
-        src: ["**/*.jsx"],
-        dest: "./src/app",
-        ext: ".js"
-      }
-    },
-
-    esteWatch: {
-      options: {
-        livereload: {
-            enabled: false
+        connect: {
+            server: {
+                options: {
+                    port: 8080,
+                    base: "./src",
+                    keepalive: true
+                }
+            }
         },
-        dirs: [
-          "./src/app/**/"
-        ]
-      },
-      jsx: function(filepath) {
-        return ["react"];
-      }
-    }
-  };
 
-  grunt.initConfig(config);
+        react: {
+            options: {
+                extension: ".jsx",
+                ignoreMTime: false
+            },
+            files: {
+                expand: true,
+                cwd: "./src/app",
+                src: ["**/*.jsx"],
+                dest: "./src/app",
+                ext: ".js"
+            }
+        },
 
-  grunt.loadNpmTasks("grunt-contrib-connect");
-  grunt.loadNpmTasks("grunt-react");
-  grunt.loadNpmTasks('grunt-este-watch');
+        esteWatch: {
+            options: {
+                livereload: {
+                    enabled: false
+                },
+                dirs: [
+                    "./src/app/**/"
+                ]
+            },
+            jsx: function(filepath) {
+                return ["react"];
+            }
+        }
+    };
 
-  grunt.registerTask("default", ["react"]);
+    grunt.initConfig(config);
+
+    grunt.loadNpmTasks("grunt-contrib-connect");
+    grunt.loadNpmTasks("grunt-react");
+    grunt.loadNpmTasks('grunt-este-watch');
+
+    grunt.registerTask("default", ["react"]);
 };
--- src/app/app.jsx
+++ src/app/app.jsx
@@ -3,9 +3,9 @@
  */
 define(["lodash", "react"], function(_, React) {
     var AppContainer = React.createClass({
-       render: function() {
-           return <h1>REACT IS LIVE!</h1>;
-       }
+        render: function() {
+            return <h1>REACT IS LIVE!</h1>;
+        }
     });
 
     return AppContainer;
